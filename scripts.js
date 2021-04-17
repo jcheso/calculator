4c8109dcf3db61650d0758ebb3f3e5992851aea8
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-04-17 13:53:14 +0800

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+/* our calculator is going to contain functions 
+for all of the basic math operators you typically find on simple calculators, 
+so start by creating functions for the following items and testing them in your browser’s console.
+add
+subtract
+multiply
+divide
+
+Create a new function operate that takes an operator 
+and 2 numbers and then calls one of the above functions on the numbers.
+ */
+
+/* Create the functions that populate the display when you click the number buttons… 
+you should be storing the ‘display value’ in a variable somewhere for use in the next step. */
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const sum = (a, b) => a + b;
+
+const multiply = (a, b) => a*b;
+
+console.log(sum(2,2))
