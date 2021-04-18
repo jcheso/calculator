0a006dc9de3df475140923b8704563e94a1a3d99
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-04-18 11:09:35 +0800

    Added calculator functionality

--- scripts.js
+++ scripts.js
@@ -1,51 +1,54 @@
 // Calculator operators
-const add = (a, b) => a + b;
+const divide = (a, b) => a / b;
 const subtract = (a, b) => a - b;
 const sum = (a, b) => a + b;
 const multiply = (a, b) => a * b;
 
 // Calculator Function
 function calculate(operator, a, b) {
+  a = parseFloat(a);
+  b = parseFloat(b);
   switch (operator) {
-    case "add":
-      result = add(a, b);
-      break;
+    case "divide":
+      calcResult = divide(a, b);
+      return calcResult;
     case "subtract":
-      result = subtract(a, b);
-      break;
+      calcResult = subtract(a, b);
+      return calcResult;
     case "sum":
-      result = sum(a, b);
-      break;
+      calcResult = sum(a, b);
+      return calcResult;
     case "multiply":
-      result = multiply(a, b);
-      break;
+      calcResult = multiply(a, b);
+      return calcResult;
     default:
-      break;
+      return calcResult;
   }
 }
 
-// Add event listeners to the buttons
-// buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll("button");
+var currentData = {
+  valueA: "",
+  operator: "",
+};
 
-// we use the .forEach method to iterate through each button
+// Add event listeners to the buttons
+const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     console.log(button.value);
     switch (button.id) {
-      case "number":
+      case "number": // If a number is selected run the newNumber script to update the display
         newNumber(button.value);
         break;
-      case "operator":
-        const currentValue = getCurrentValue();
-        valueA = calculateResult(operator, currentValue);
-        clearDisplay();
-        return valueA;
-      case "clear":
+      case "operator": //If an operator is selected, check if a number has been entered and store both in memory
+        currentData = operateInput(button.value, currentData);
+        break;
+      case "clear": // Wipe the display and reset any values in memory
+        clearMemory();
         clearDisplay();
         break;
-      case "delete":
-        removeNumber();
+      case "delete": // Delete the last number input to the display
+        deleteNumber();
         break;
       default:
         break;
@@ -54,50 +57,91 @@ buttons.forEach((button) => {
   });
 });
 
+// Get the current value entered into the display
 function getCurrentValue() {
   const displayValue = document.querySelectorAll("#display");
-  const currentValue = displayValue[0].innerText;
+  let currentValue = displayValue[0].innerText;
   return currentValue;
 }
 
+// Update the display to the value passed in
 function updateDisplay(newValue) {
   document.getElementById("display").innerText = newValue;
 }
 
+// Clear the display visually, does not clear any values in memory
 function clearDisplay() {
   document.getElementById("display").innerText = "";
 }
 
-function removeNumber() {
-  const currentValue = getCurrentValue();
+// Clear the display and all objects in memory
+function clearMemory() {
+  currentData["valueA"] = "";
+  currentData["operator"] = "";
+  currentValue = "";
+}
+
+// Delete the last number entered and update the display
+function deleteNumber() {
+  currentValue = getCurrentValue();
   const newValue = currentValue.slice(0, -1);
   updateDisplay(newValue);
 }
 
+// Add the number selected to the current value and update the display.
+// Check to see if it is a decimal and if there is already a decimal in the string - if so do not update string
 function newNumber(inputValue) {
-  const currentValue = getCurrentValue();
-  const newValue = currentValue + inputValue;
+  let newValue = ''
+  currentValue = getCurrentValue();
+  if (inputValue == '.' && currentValue.indexOf('.') > -1){
+    newValue = currentValue
+  }
+  else{
+    newValue = currentValue + inputValue;
+  }
   updateDisplay(newValue);
 }
 
-// function storeOperation(operator) {
-//   const lastOperation = operator;
-// }
+function operateInput(operator, currentData) {
+  // Take the number on the display and the operator entered
+  currentValue = getCurrentValue();
 
-// function storeNumber() {
-//   const lastNumber = getCurrentValue();
-// }
-
-let valueA = null;
-
-function calculateResult(operator, currentValue) {
-  if (valueA != null) {
-    currentValue = parseInt(currentValue);
-    valueA = calculate(operator, currentValue);
-  } else {
-    valueA = parseInt(currentValue);
+  // Check if there is already a number and operator in memory.
+  // If there is no number and operator in memory, store the value of result + operator in the object
+  if (currentData["operator"] == "") {
+    currentData["valueA"] = currentValue;
+    currentData["operator"] = operator;
+    clearDisplay();
   }
-  return valueA;
+  // Divide by zero check
+  else if(currentData["operator"] == 'divide' && parseFloat(currentValue) == 0){
+    updateDisplay('ERROR - Cannot divide by zero!')
+    clearMemory()
+  }
+  // If valueA exists, perform the operation of A operate B first - then store the value of result + operator in the object.
+  else if (operator != "calculate") {
+    calcResult = calculate(
+      currentData["operator"],
+      currentData["valueA"],
+      currentValue
+    );
+    currentData["valueA"] = calcResult;
+    currentData["operator"] = operator;
+    clearDisplay();
+
+    //If the equals button is pressed calculate the equation and display the answer on the screen
+  } else {
+    calcResult = calculate(
+      currentData["operator"],
+      currentData["valueA"],
+      currentValue
+    );
+    currentData["valueA"] = calcResult;
+    currentData["operator"] = "";
+    updateDisplay(calcResult.toFixed(2));
+    currentValue = '';
+  }
+  return currentData;
 }
 
 // function storeOperation() {
